56d8357931f28583e7d97685e2850b3d9c6e4605
jhor <j.horyd@outlook.com>
2023-11-19 17:46:16 +0100

    Handle basic sql connection for adding new test case

--- src/main/java/components/addnewtestcase/SaveTestCaseButton.java
+++ src/main/java/components/addnewtestcase/SaveTestCaseButton.java
@@ -1,10 +1,14 @@
 package components.addnewtestcase;
 
 import components.OptiTextField;
+import core.PropertiesHandler;
 import panels.addnewtestcase.*;
 import sections.CreateNewTestCaseMenu;
 
 import javax.swing.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -22,7 +26,7 @@ public class SaveTestCaseButton extends JButton {
             if (listOfNotFieldWithDefaultValues.isEmpty()) {
                 int confirmationDialogResult = showConfirmationDialog();
                 if (confirmationDialogResult == JOptionPane.YES_OPTION) {
-                    printTestCaseDetails();
+                    uploadTestCaseToDataBase();
                 }
             } else {
                 showErrorMessage(listOfNotFieldWithDefaultValues);
@@ -53,8 +57,53 @@ public class SaveTestCaseButton extends JButton {
         return JOptionPane.showConfirmDialog(null, "Would You Like to Save this Test Case?", "Warning", JOptionPane.YES_NO_OPTION);
     }
 
-    private void printTestCaseDetails() {
-        testCaseDetails.forEach((key, value) -> System.out.println(key + ": " + value));
+    private void uploadTestCaseToDataBase() {
+        String testCaseTitleValue = TestCaseTitlePanel.getTestCaseTitleTextField();
+        String testCaseDescriptionValue = TestCaseDescriptionPanel.getTestCaseDescriptionTextField();
+        String testCasePriorityValue = TestCasePriorityPanel.getPriorityComboBox();
+        String testCaseTypeValue = TestCaseTypePanel.getTypeComboBox();
+        List<OptiTextField> stepsTextFieldFromStepsContainerPanel = CreateNewTestCaseMenu.getStepsTextFieldFromStepsContainerPanel();
+        List<String> stepsTextFieldFromStepsContainerPanelValues = stepsTextFieldFromStepsContainerPanel.stream()
+                .map(OptiTextField::getText)
+                .toList();
+        String testCaseExpectedResultsValue = TestCaseExpectedResultsPanel.getTestCaseExpectedResultsTextField();
+
+        try {
+            PropertiesHandler propertiesHandler = new PropertiesHandler();
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection connection = DriverManager.getConnection(
+                    propertiesHandler.getSqlUrl(), propertiesHandler.getSqlUser(), propertiesHandler.getSqlPassword()
+            );
+            Statement statement = connection.createStatement();
+
+            StringBuilder stepsConcatenated = new StringBuilder();
+            for (String step : stepsTextFieldFromStepsContainerPanelValues) {
+                stepsConcatenated.append(step).append("\n"); // Separating steps by newline
+            }
+
+            // Constructing the SQL INSERT statement
+            String insertQuery = "INSERT INTO test_cases " +
+                    "(testcase_name, testcase_description, testcase_priority, testcase_type, step, testcase_expected_results) " +
+                    "VALUES (?, ?, ?, ?, ?, ?)";
+
+            // Creating prepared statement for SQL injection prevention and better performance
+            java.sql.PreparedStatement preparedStatement = connection.prepareStatement(insertQuery);
+            preparedStatement.setString(1, testCaseTitleValue);
+            preparedStatement.setString(2, testCaseDescriptionValue);
+            preparedStatement.setString(3, testCasePriorityValue);
+            preparedStatement.setString(4, testCaseTypeValue);
+            preparedStatement.setString(5, stepsConcatenated.toString()); // Inserting steps as concatenated string
+            preparedStatement.setString(6, testCaseExpectedResultsValue);
+
+            // Executing the insert query
+            preparedStatement.executeUpdate();
+
+            // Closing resources
+            preparedStatement.close();
+            connection.close();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
     }
 
     private void showErrorMessage(List<String> listOfNotFieldWithDefaultValues) {
